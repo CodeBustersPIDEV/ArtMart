5d6b191d20ba016f402d944fead8e1ca2ac1b795
samar0507 <79337944+samar0507@users.noreply.github.com>
2023-02-12 00:29:45 +0100

    update interface user

--- ArtMart_JAVA_Build/src/com/artmart/interfaces/IUserService.java
+++ ArtMart_JAVA_Build/src/com/artmart/interfaces/IUserService.java
@@ -1,5 +1,27 @@
 package com.artmart.interfaces;
-
+import com.artmart.models.User;
+import com.artmart.models.Client;
+import com.artmart.models.Admin;
+import com.artmart.models.Artist;
 public interface IUserService {
+    User createAccountU(User user);
+    User viewAccountDetailsU(int user_id);
+    boolean deleteAccountU(int userId);
+    User updateAccountU(int user_id, User user);
     
+     Client createAccountC(Client client);
+    Client viewAccountDetailsC(int client_id);
+    boolean deleteAccountC(int client_d);
+    Client updateAccountC(int client_id,Client client);
+    
+    Artist createAccountAr(Artist artist);
+    Artist viewAccountDetailsAr(int artist_id);
+    boolean deleteAccountAr(int artist_d);
+    Artist updateAccountAr(int atist_id, Artist artist);
+    
+     Admin createAccountA(Admin admin);
+    Admin viewAccountDetailsA(int admin_id);
+    void viewListOfUsersA();
+    boolean deleteAccountA(int admin_id);
+    Admin updateAccountA(int admin_id, Admin admin);
 }
--- ArtMart_JAVA_Build/src/com/artmart/services/UserService.java
+++ ArtMart_JAVA_Build/src/com/artmart/services/UserService.java
@@ -1,6 +1,97 @@
 package com.artmart.services;
+import com.artmart.connectors.SQLConnection;
 import com.artmart.interfaces.IUserService;
+import com.artmart.models.Admin;
+import com.artmart.models.Artist;
+import com.artmart.models.Client;
+import com.artmart.models.User;
 
 public class UserService implements IUserService{
-    
+    SQLConnection connection = SQLConnection.getInstance();
+
+    @Override
+    public User createAccountU(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public User viewAccountDetailsU(int user_id) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public boolean deleteAccountU(int userId) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    @Override
+    public User updateAccountU(int user_id, User user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Client createAccountC(Client client) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Client viewAccountDetailsC(int client_id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteAccountC(int client_d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Client updateAccountC(int client_id, Client client) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Artist createAccountAr(Artist artist) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Artist viewAccountDetailsAr(int artist_id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteAccountAr(int artist_d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Artist updateAccountAr(int atist_id, Artist artist) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Admin createAccountA(Admin admin) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Admin viewAccountDetailsA(int admin_id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void viewListOfUsersA() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteAccountA(int admin_id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Admin updateAccountA(int admin_id, Admin admin) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
