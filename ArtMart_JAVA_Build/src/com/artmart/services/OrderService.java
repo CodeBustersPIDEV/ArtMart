259b524c37193835112454190631a9559ce31fab
MeherBenSalem <meher.bensalem@esprit.tn>
2023-02-11 20:35:09 +0100

    Update OrderService.java

--- ArtMart_JAVA_Build/src/com/artmart/services/OrderService.java
+++ ArtMart_JAVA_Build/src/com/artmart/services/OrderService.java
@@ -1,6 +1,7 @@
 package com.artmart.services;
+import com.artmart.connectors.SQLConnection;
 import com.artmart.interfaces.IOrderService;
 
 public class OrderService implements IOrderService{
-    
+    SQLConnection connection = SQLConnection.getInstance();
 }
