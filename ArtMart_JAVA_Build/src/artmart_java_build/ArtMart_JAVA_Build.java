7a64aa797edfc96fcf1b9c517d7ed6d538e87149
MeherBenSalem <meher.bensalem@esprit.tn>
2023-02-11 22:19:02 +0100

    Revert "Merge branch 'main' into Ghassen"
    This reverts commit 3a111cf8f1fed6e3681002859355b47dd81af57b, reversing
    changes made to 76dcb045deb76e4538f538fdbc291457f4752748.

--- ArtMart_JAVA_Build/src/artmart_java_build/ArtMart_JAVA_Build.java
+++ ArtMart_JAVA_Build/src/artmart_java_build/ArtMart_JAVA_Build.java
@@ -2,14 +2,13 @@ package artmart_java_build;
 
 import com.artmart.connectors.SQLConnection;
 import java.sql.SQLException;
-import java.sql.Connection;
 
 public class ArtMart_JAVA_Build {
 
     public static void main(String[] args) {
         // TODO code application logic here
         try{
-            Connection con = SQLConnection.getInstance().getConnection();
+            SQLConnection.getInstance().connect();
         }catch(SQLException e){
             System.err.println(e.fillInStackTrace());
         }
