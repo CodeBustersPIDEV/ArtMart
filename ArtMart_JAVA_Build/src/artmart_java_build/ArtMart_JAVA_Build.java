43fa4acf3e4be166637e0d26b7c91aedfa7bee21
MeherBenSalem <meher.bensalem@esprit.tn>
2023-02-11 22:10:37 +0100

    Updated SQLConnectionClass
    ista3mlou this line bich tekhdou il cmx  Connection con = SQLConnection.getInstance().getConnection();

--- ArtMart_JAVA_Build/src/artmart_java_build/ArtMart_JAVA_Build.java
+++ ArtMart_JAVA_Build/src/artmart_java_build/ArtMart_JAVA_Build.java
@@ -2,13 +2,14 @@ package artmart_java_build;
 
 import com.artmart.connectors.SQLConnection;
 import java.sql.SQLException;
+import java.sql.Connection;
 
 public class ArtMart_JAVA_Build {
 
     public static void main(String[] args) {
         // TODO code application logic here
         try{
-            SQLConnection.getInstance().connect();
+            Connection con = SQLConnection.getInstance().getConnection();
         }catch(SQLException e){
             System.err.println(e.fillInStackTrace());
         }
