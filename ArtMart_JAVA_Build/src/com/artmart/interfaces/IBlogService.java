9671fff2bece359cb8e8de3e1910c05f7bf292df
MarwenTouati1 <marwen.touati@esprit.tn>
2023-02-12 22:39:37 +0100

    CRUD BlogCategories
    different of the normal categories for products and custom products

--- ArtMart_JAVA_Build/src/com/artmart/interfaces/IBlogService.java
+++ ArtMart_JAVA_Build/src/com/artmart/interfaces/IBlogService.java
@@ -1,5 +1,6 @@
 package com.artmart.interfaces;
 import com.artmart.models.Blog;
+import com.artmart.models.BlogCategories;
 import com.artmart.models.Comment;
 import java.util.List;
 
@@ -16,5 +17,12 @@ public interface IBlogService {
     public Comment getOneComment(int comment_id); 
     public List<Comment> getAllComments(); 
     public boolean  updateComment(int comment_id, Comment editedComment); 
-    public boolean  deleteComment(int comment_id); 
+    public boolean  deleteComment(int comment_id);
+    
+    //Blog Categories DAO
+    public int addBlogCategories(BlogCategories bc); 
+    public BlogCategories getOneBlogCategory(int blogsCategory_id); 
+    public List<BlogCategories> getAllBlogCategories(); 
+    public boolean  updateBlogCategory(int blogsCategory_id, BlogCategories editedBlogCat); 
+    public boolean  deleteBlogCategory(int blogsCategory_id); 
 }
