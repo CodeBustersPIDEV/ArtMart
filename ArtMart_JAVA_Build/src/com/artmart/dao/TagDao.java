d4488c3fda32f2dec014df583eb5c3bf2ec93cb7
MarwenTouati1 <marwen.touati@esprit.tn>
2023-02-13 22:21:33 +0100

    Media CRUD,Tag CRUD

--- ArtMart_JAVA_Build/src/com/artmart/dao/TagDao.java
+++ ArtMart_JAVA_Build/src/com/artmart/dao/TagDao.java
@@ -5,54 +5,53 @@
  */
 package com.artmart.dao;
 
-import com.artmart.interfaces.IBlogCategoriesDao;
-import com.artmart.models.BlogCategories;
+import com.artmart.interfaces.ITagDao;
+import com.artmart.models.Tag;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 /**
  *
  * @author marwen
  */
-public class BlogCategoriesDao implements IBlogCategoriesDao{
- 
+public class TagDao implements ITagDao{
+    
     private Connection connection;
 
-    public BlogCategoriesDao(Connection connection) {
+    public TagDao(Connection connection) {
         this.connection = connection;
     }
 
     @Override
-    public int addBlogCategories(BlogCategories bc) {
+    public int addTag(Tag t) {
 try {
-      String sql = "INSERT INTO blogcategories ( name) VALUES (?)";
+      String sql = "INSERT INTO tags ( name) VALUES (?)";
       PreparedStatement st = connection.prepareStatement(sql);
-      st.setString(1, bc.getName());
+      st.setString(1, t.getName());
       st.executeUpdate();
       return 1;
     } catch (SQLException e) {
         System.err.println("Error occured");
          e.printStackTrace();
         return 0;
-    }     }
+    }         }
 
     @Override
-    public BlogCategories getOneBlogCategory(int blogsCategory_id) {
-BlogCategories blogCategoryFound = null;
+    public Tag getOneTag(int tag_id) {
+Tag tagFound = null;
     try {
            PreparedStatement statement = connection.prepareStatement(
-                "SELECT * FROM blogcategories WHERE blogsCategory_id = ?"
+                "SELECT * FROM tags WHERE tags_ID = ?"
             );
-            statement.setInt(1, blogsCategory_id);
+            statement.setInt(1, tag_id);
 
             ResultSet result = statement.executeQuery();
             if (result.next()) {
-                blogCategoryFound = new BlogCategories(
-                result.getInt("blogsCategory_id"),
+                tagFound = new Tag(
+                result.getInt("tag_id"),
                 result.getString("name")
         );
             }
@@ -60,18 +59,18 @@ BlogCategories blogCategoryFound = null;
       System.err.println("Error occured");
       e.printStackTrace();
     }
-    return blogCategoryFound;    }
+    return tagFound;       }
 
     @Override
-    public List<BlogCategories> getAllBlogCategories() {
- List<BlogCategories> blogCategoriesList = new ArrayList<>();
+    public List<Tag> getAllTags() {
+List<Tag> tagsList = new ArrayList<>();
     try {
-      String sql = "SELECT * FROM blogcategories";
+      String sql = "SELECT * FROM tags";
       PreparedStatement st = connection.prepareStatement(sql);
       ResultSet rs = st.executeQuery(sql);
       while (rs.next()) {
-        blogCategoriesList.add(new BlogCategories(
-          rs.getInt("blogsCategory_id"),
+        tagsList.add(new Tag(
+          rs.getInt("tags_ID"),
           rs.getString("name")
         ));
       }
@@ -80,33 +79,33 @@ BlogCategories blogCategoryFound = null;
            e.printStackTrace();
 
     }
-    return blogCategoriesList;     }
+    return tagsList;         }
 
     @Override
-    public boolean updateBlogCategory(int blogsCategory_id, BlogCategories editedBlogCat) {
+    public boolean updateTag(int tag_id, Tag editedTag) {
 try {
-      String sql = "UPDATE blogcategories SET name = ? WHERE blogsCategory_id = ?";
+      String sql = "UPDATE tags SET name = ? WHERE tags_ID = ?";
       PreparedStatement st = connection.prepareStatement(sql);
-      st.setString(1, editedBlogCat.getName());
-      st.setInt(2, blogsCategory_id);
+      st.setString(1, editedTag.getName());
+      st.setInt(2, tag_id);
       st.executeUpdate();
       return true;
     } catch (SQLException e) {
      System.err.println("Error occured");
      return false;
-    }      }
+    }          }
 
     @Override
-    public boolean deleteBlogCategory(int blogsCategory_id) {
+    public boolean deleteTag(int tag_id) {
 try {
-      String sql = "DELETE FROM blogcategories WHERE blogsCategory_id = ?";
+      String sql = "DELETE FROM tags WHERE tags_ID = ?";
       PreparedStatement st = connection.prepareStatement(sql);
-      st.setInt(1, blogsCategory_id);
+      st.setInt(1, tag_id);
       st.executeUpdate();
       return true;
     } catch (SQLException e) {
      System.err.println("Error occured");
      return false;
-    }       }
-
+    }           }
+    
 }
--- ArtMart_JAVA_Build/src/com/artmart/services/BlogService.java
+++ ArtMart_JAVA_Build/src/com/artmart/services/BlogService.java
@@ -3,10 +3,14 @@ import com.artmart.connectors.SQLConnection;
 import com.artmart.dao.BlogDao;
 import com.artmart.dao.CommentDao;
 import com.artmart.dao.BlogCategoriesDao;
+import com.artmart.dao.MediaDao;
+import com.artmart.dao.TagDao;
 import com.artmart.interfaces.IBlogService;
 import com.artmart.models.Blog;
 import com.artmart.models.BlogCategories;
 import com.artmart.models.Comment;
+import com.artmart.models.Media;
+import com.artmart.models.Tag;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
@@ -18,6 +22,8 @@ public class BlogService implements IBlogService{
     private BlogDao blogDao;
     private CommentDao commentDao;    
     private BlogCategoriesDao blogCategories;
+    private TagDao tag;
+    private MediaDao media;
 
 
     public BlogService() {
@@ -26,6 +32,8 @@ public class BlogService implements IBlogService{
         this.blogDao = new BlogDao(this.connection);
         this.commentDao = new CommentDao(this.connection);
         this.blogCategories = new BlogCategoriesDao(this.connection);
+        this.tag = new TagDao(this.connection);
+        this.media = new MediaDao(this.connection);
         }catch(SQLException e){
             System.err.print(e.getMessage());
         }
@@ -106,6 +114,56 @@ return this.commentDao.deleteComment(comment_id);     }
     @Override
     public boolean deleteBlogCategory(int blogsCategory_id) {
         return this.blogCategories.deleteBlogCategory(blogsCategory_id);
+    }
+
+    @Override
+    public int addTag(Tag t) {
+        return this.tag.addTag(t);
+    }
+
+    @Override
+    public Tag getOneTag(int tag_id) {
+        return this.tag.getOneTag(tag_id);
+    }
+
+    @Override
+    public List<Tag> getAllTags() {
+        return this.tag.getAllTags();
+    }
+
+    @Override
+    public boolean updateTag(int tag_id, Tag editedTag) {
+        return this.tag.updateTag(tag_id,editedTag);
+    }
+
+    @Override
+    public boolean deleteTag(int tag_id) {
+        return this.tag.deleteTag(tag_id);
+    }
+
+    @Override
+    public int addMedia(Media m) {
+        return this.media.addMedia(m);
+    }
+
+    @Override
+    public Media getOneMedia(int media_id) {
+        return this.media.getOneMedia(media_id);
+    }
+
+    @Override
+    public List<Media> getAllMedias() {
+        return this.media.getAllMedias();
+    }
+
+    @Override
+    public boolean updateMedia(int media_id, Media editedMedia) {
+        return this.media.updateMedia(media_id,editedMedia);
+    }
+
+    @Override
+    public boolean deleteMedia(int media_id) {
+        return this.media.deleteMedia(media_id);
     }
 
 }
