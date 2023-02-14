dd1a33c48daff75c43625611568083e0c7236c89
MeherBenSalem <meher.bensalem@esprit.tn>
2023-02-14 12:02:40 +0100

    All_DAOs_Update
    Crud Done all DAOs

--- ArtMart_JAVA_Build/src/com/artmart/dao/PaymentOptionDao.java
+++ ArtMart_JAVA_Build/src/com/artmart/dao/PaymentOptionDao.java
@@ -1,10 +1,10 @@
 package com.artmart.dao;
 
 import com.artmart.connectors.SQLConnection;
-import java.sql.Connection;
 import com.artmart.interfaces.*;
 import com.artmart.models.PaymentOption;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PaymentOptionDao implements IPaymentOptionDao{
@@ -18,29 +18,78 @@ public class PaymentOptionDao implements IPaymentOptionDao{
             System.err.print(e.getMessage());
         }
     }
-   @Override
-    public int createPaymentOption(PaymentOption paymentOption) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    @Override
+    public int createPaymentOption(PaymentOption paymentOption){
+        String sql = "INSERT INTO payment_option (name, available_countries) VALUES (?, ?)";
+        try (PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, paymentOption.getName());
+            stmt.setString(2, paymentOption.getAvailableCountries());
+            return stmt.executeUpdate();
+        }catch(SQLException e){
+            System.err.print(e.getMessage());
+        }
+        return 0;
     }
 
     @Override
-    public PaymentOption getPaymentOption(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public PaymentOption getPaymentOption(int id){
+        String sql = "SELECT * FROM payment_option WHERE id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    return new PaymentOption(rs.getInt("id"), rs.getString("name"), rs.getString("available_countries"));
+                } else {
+                    return null;
+                }
+            }
+        }catch(SQLException e){
+            System.err.print(e.getMessage());
+        }
+        return null;
     }
 
     @Override
-    public List<PaymentOption> getPaymentOptions() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<PaymentOption> getPaymentOptions(){
+        List<PaymentOption> paymentOptions = new ArrayList<>();
+        String sql = "SELECT * FROM payment_option";
+        try (PreparedStatement stmt = connection.prepareStatement(sql); ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                paymentOptions.add(new PaymentOption(rs.getInt("id"), rs.getString("name"), rs.getString("available_countries")));
+            }
+        }catch(SQLException e){
+            System.err.print(e.getMessage());
+        }
+        return paymentOptions;
     }
 
     @Override
-    public boolean updatePaymentOption(PaymentOption paymentOption) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean updatePaymentOption(PaymentOption paymentOption){
+        String sql = "UPDATE payment_option SET name = ?, available_countries = ? WHERE id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, paymentOption.getName());
+            stmt.setString(2, paymentOption.getAvailableCountries());
+            stmt.setInt(3, paymentOption.getId());
+            int rowsUpdated = stmt.executeUpdate();
+            return rowsUpdated > 0;
+        }catch(SQLException e){
+            System.err.print(e.getMessage());
+        }
+        return false;
     }
 
     @Override
-    public boolean deletePaymentOption(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean deletePaymentOption(int id){
+        String sql = "DELETE FROM payment_option WHERE id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            int rowsDeleted = stmt.executeUpdate();
+            return rowsDeleted > 0;
+        }catch(SQLException e){
+            System.err.print(e.getMessage());
+        }
+        return false;
     }
 
 }
