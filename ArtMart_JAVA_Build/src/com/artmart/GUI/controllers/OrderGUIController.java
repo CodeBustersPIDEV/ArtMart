6d62d35229d9e76a2a4b6e95f459f46eac03c6c4
MeherBenSalem <124251820+M3herBenSalem@users.noreply.github.com>
2023-02-17 23:07:28 +0100

    Insert_SQL

--- ArtMart_JAVA_Build/src/com/artmart/GUI/controllers/OrderGUIController.java
+++ ArtMart_JAVA_Build/src/com/artmart/GUI/controllers/OrderGUIController.java
@@ -1,14 +1,20 @@
 package com.artmart.GUI.controllers;
 
+import com.artmart.models.Order;
+import com.artmart.services.OrderService;
 import java.net.URL;
+import java.sql.Date;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 
 public class OrderGUIController implements Initializable {
 
+    OrderService orderService = new OrderService();
     @FXML
     private TextField userIdTextField;
     @FXML
@@ -26,7 +32,7 @@ public class OrderGUIController implements Initializable {
     @FXML
     private TextField paymentMethodTextField;
     @FXML
-    private TextField orderDateTextField;
+    private DatePicker orderDatePickerField;
     @FXML
     private TextField totalCostTextField;
     @FXML
@@ -34,9 +40,27 @@ public class OrderGUIController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        this.placeOrderBtn.setOnAction(e->{
-            System.out.print("Hello");
-        }); 
-    }    
-    
+    }
+
+    @FXML
+    private void placeOrder(ActionEvent event) {
+        
+        String shippingAddress = this.shippingAddressStreetTextField.getText() + " "
+                + this.shippingAddressCityTextField.getText() + " "
+                + this.shippingAddressStateTextField.getText() + " "
+                + this.shippingAddressZipTextField.getText();
+
+        Order newOrder = new Order(
+                Integer.parseInt(this.userIdTextField.getText()),
+                Integer.parseInt(this.productIdTextField.getText()),
+                Integer.parseInt(this.quantityTextField.getText()),
+                shippingAddress,
+                this.paymentMethodTextField.getText(),
+                Date.valueOf(this.orderDatePickerField.getValue()),
+                Double.parseDouble(this.totalCostTextField.getText())
+        );
+        
+        this.orderService.createOrder(newOrder);
+    }
+
 }
---
+++ InsertSQL.sql
@@ -0,0 +1,65 @@
+INSERT INTO user(name, email, birthday, phoneNumber, role, username, password) VALUES ('John Doe', 'johndoe@example.com', '1990-01-01', '+1234567890', 'client', 'johndoe', 'password123');
+
+INSERT INTO artist(user_ID, nbr_artwork, bio) VALUES (1, 10, 'I am a passionate artist who loves to explore different art styles.');
+
+INSERT INTO admin(user_ID, department) VALUES (1, 'Human Resources');
+
+INSERT INTO client(user_ID, nbr_orders, nbr_demands) VALUES (1, 0, 0);
+
+INSERT INTO premium(user_ID, duration, price) VALUES (1, 30, 9.99);
+
+INSERT INTO blogs(title, content, author) VALUES ('The Importance of Art Education', 'In this blog post, I discuss the importance of art education and how it can positively impact a child’s life.', 1);
+
+INSERT INTO comments(content, author, blog_ID) VALUES ('I completely agree with your points. Art education is crucial for developing creativity and critical thinking skills.', 1, 1);
+
+INSERT INTO blogcategories(name) VALUES ('Education');
+
+INSERT INTO has_blog_category(blog_id, category_id) VALUES (1, 1);
+
+INSERT INTO tags(name) VALUES ('art education');
+
+INSERT INTO blog_tags(blog_id, tag_id) VALUES (1, 1);
+
+INSERT INTO media(blog_id, file_name, file_type, file_path) VALUES (1, 'art_education.jpg', 'image/jpeg', '/media/art_education.jpg');
+
+INSERT INTO categories(name) VALUES ('Furniture');
+
+INSERT INTO product(category_ID, name, description, dimensions, weight, material, image) VALUES (1, 'Modern Sofa', 'A comfortable and stylish sofa for any living room.', '72" L x 30" H x 36" D', 75.50, 'Leather', 'sofa.jpg');
+
+INSERT INTO readyProduct(price, product_ID) VALUES (599, 1);
+
+INSERT INTO productReview(ready_product_ID, username, title, text, rating) VALUES (1, 'johndoe', 'Great Sofa!', 'This sofa is comfortable and looks great in my living room. Highly recommend!', 5);
+
+INSERT INTO customProduct(product_ID) VALUES (1);
+
+INSERT INTO chat(client_ID, custom_product_ID, artist_ID, history) VALUES (1, 1, 1, 'Hi, I am interested in purchasing a custom version of the Modern Sofa.');
+
+INSERT INTO shippingOption(Name, Carrier, ShippingSpeed, ShippingFee, AvailableRegions) VALUES ('Standard Shipping', 'UPS', '5-7 business days', 10.00, 'US');
+
+INSERT INTO paymentOption(Name, AvailableCountries) VALUES ('Credit Card', 'US');
+
+INSERT INTO order(UserID, ProductID, Quantity, ShippingAddress, PaymentMethod, OrderDate, TotalCost) VALUES (1, 1, 1, 1, 1, '2023-02-17', 609.00);
+
+INSERT INTO receipt (orderID, productID, quantity, price, tax, totalCost, date) 
+VALUES (1, 2, 1, 10.0, 1.5, 11.5, '2023-02-16');
+
+INSERT INTO wishlist (userID, productID, date) 
+VALUES (1, 2, '2023-02-16');
+
+INSERT INTO orderRefund (orderID, refundAmount, reason, date) 
+VALUES (1, 50.0, 'Wrong product received', '2023-02-16');
+
+INSERT INTO salesReport (productID, totalSales, averageSalesPerDay, date) 
+VALUES (1, 5000.0, 200.0, '2023-02-16');
+
+INSERT INTO event (userID, name, location, type, description, entryFee, capacity, startDate, endDate) 
+VALUES (1, 'Spring Festival', 'Central Park', 'Festival', 'Join us for a day of music, food, and fun!', 10.0, 1000, '2023-05-20 10:00:00', '2023-05-20 22:00:00');
+
+INSERT INTO activity (eventID, startDate, endDate, title, host) 
+VALUES (1, '2023-03-10 10:00:00', '2023-03-10 11:00:00', 'Yoga Class', 'John Doe');
+
+INSERT INTO eventReport (eventID, attendance) 
+VALUES (1, 50);
+
+INSERT INTO feedback (eventReportID, rating, comment) 
+VALUES (1, 4, "The event was well-organized and enjoyable.");
