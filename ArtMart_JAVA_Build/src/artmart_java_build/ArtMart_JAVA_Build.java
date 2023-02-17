194958dc1ef921c1ce7abe406ab772cb44ce1cfd
M3herBenSalem <124251820+M3herBenSalem@users.noreply.github.com>
2023-02-17 11:38:59 +0100

    Add_JavaFx_Libraries
    wiw

--- ArtMart_JAVA_Build/src/artmart_java_build/ArtMart_JAVA_Build.java
+++ ArtMart_JAVA_Build/src/artmart_java_build/ArtMart_JAVA_Build.java
@@ -1,8 +1,25 @@
 package artmart_java_build;
 
-public class ArtMart_JAVA_Build {
+import javafx.application.*;
+import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.fxml.FXMLLoader;
+
+public class ArtMart_JAVA_Build extends Application {
+    
+    private Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/artmart/GUI/views/Main_View.fxml"));
+        Parent page = loader.load();
+        Scene scene = new Scene(page);
+        this.primaryStage.setScene(scene);
+        this.primaryStage.show();
+    }
 
     public static void main(String[] args) {
-     
+        launch(args);
     }
 }
