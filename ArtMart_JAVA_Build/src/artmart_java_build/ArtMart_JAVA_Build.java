100c50925736aa91660ec8626bcf08cba039097c
AmirCW <80427194+MedAmirSoltani@users.noreply.github.com>
2023-02-18 18:27:47 +0100

    all pulled

--- ArtMart_JAVA_Build/src/artmart_java_build/ArtMart_JAVA_Build.java
+++ ArtMart_JAVA_Build/src/artmart_java_build/ArtMart_JAVA_Build.java
@@ -1,44 +1,22 @@
 package artmart_java_build;
 
-import java.sql.Connection;
-import com.artmart.connectors.SQLConnection;
-import com.artmart.dao.CategoriesDao;
-import com.artmart.dao.ChatDao;
-import com.artmart.dao.CustomProductDao;
-import com.artmart.dao.ProductDao;
-import com.artmart.models.Categories;
-import com.artmart.models.Chat;
-import com.artmart.models.CustomProduct;
-import com.artmart.models.Event;
-import com.artmart.models.Product;
-import com.artmart.services.EventService;
-import java.sql.Date;
-import java.sql.SQLException;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.fxml.FXMLLoader;
 
+public class ArtMart_JAVA_Build extends Application{
 
-public class ArtMart_JAVA_Build {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/artmart/GUI/views/MainView.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        CustomProductDao cc = new CustomProductDao();
-        ProductDao x = new ProductDao();
-        ChatDao y = new ChatDao();
-        CategoriesDao z = new CategoriesDao();
-        EventService w = new EventService();
-        
-     
-        try{
-            Connection con = SQLConnection.getInstance().getConnection();
-            System.out.println("successfully connected ");
-       //  z.createCategories(new Categories("amir"));
-//System.out.println(z.getAllCategories());
-//System.out.println(cc.getCustomProductById(2));
-//System.out.println(y.getAllChats());
-     cc.createCustomProduct(new Product(1, "amir", "soltani", "Test",2, "Test", "Test"));
-//x.updateProduct(9, new Product(1, "amwaaaaaaaaaaaaaair", "haaaaaaaaaa", "Test",2, "Test", "Test"));
-cc.deleteCustomProduct(21);
-        }catch(SQLException e){
-            System.err.println(e.fillInStackTrace());
-             System.out.println("connection failed ");
-        }
+        launch(args);
     }
 }
