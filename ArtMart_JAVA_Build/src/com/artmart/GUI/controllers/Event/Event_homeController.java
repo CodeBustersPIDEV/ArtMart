b2640902e5eb8cc289ca619e7036350ddae7b70c
Ghassen-Zayoud <ghassen.zayoud@esprit.tn>
2023-02-20 01:16:57 +0100

    did small changes to gui

--- ArtMart_JAVA_Build/src/com/artmart/GUI/controllers/Event/Event_homeController.java
+++ ArtMart_JAVA_Build/src/com/artmart/GUI/controllers/Event/Event_homeController.java
@@ -14,6 +14,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 /**
@@ -22,6 +23,9 @@ import javafx.stage.Stage;
  * @author GhassenZ
  */
 public class Event_homeController implements Initializable {
+
+    @FXML
+    private Button btnGoToAddEvent;
 
     /**
      * Initializes the controller class.
