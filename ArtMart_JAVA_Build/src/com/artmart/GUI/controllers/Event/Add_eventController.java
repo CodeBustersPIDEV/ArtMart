848931708298de8e96d3f191713966ae718b6b2c
Ghassen-Zayoud <ghassen.zayoud@esprit.tn>
2023-02-20 01:11:58 +0100

    added AddEventController logic

--- ArtMart_JAVA_Build/src/com/artmart/GUI/controllers/Event/Add_eventController.java
+++ ArtMart_JAVA_Build/src/com/artmart/GUI/controllers/Event/Add_eventController.java
@@ -4,8 +4,12 @@
  */
 package com.artmart.GUI.controllers.Event;
 
+import com.artmart.models.Event;
+import com.artmart.services.EventService;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,6 +18,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
@@ -22,6 +32,41 @@ import javafx.stage.Stage;
  * @author GhassenZ
  */
 public class Add_eventController implements Initializable {
+    
+    private EventService es;
+    //private int eventID;
+    private String name;
+    private String location;
+    private String type;
+    private String description;
+    private double entryFee;
+    private int capacity;
+    private Date startDate;
+    private Date endDate;
+    //private int userID;
+    private int userID = 1;
+
+
+    @FXML
+    private Button btnAddEvent;
+    @FXML
+    private Button btnCancelEvent;
+    @FXML
+    private TextField txtLocation;
+    @FXML
+    private TextField txtName;
+    @FXML
+    private ComboBox comboBoxType = new ComboBox();
+    @FXML
+    private TextField txtCapacity;
+    @FXML
+    private TextField txtEntryFee;
+    @FXML
+    private TextArea txtAreaDescription;
+    @FXML
+    private DatePicker dpEndDate;
+    @FXML
+    private DatePicker dpStartDate;
 
     /**
      * Initializes the controller class.
@@ -29,6 +74,7 @@ public class Add_eventController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        comboBoxType.getItems().addAll("Auction", "Art fair", "Open Gallery", "Exhibition");
     }    
 
     @FXML
@@ -45,5 +91,41 @@ public class Add_eventController implements Initializable {
             System.out.print(e.getMessage());
         }        
     }
+    
+    @FXML
+    private void onAddEvent(ActionEvent event) {
+        name = txtName.getText();
+        location = txtLocation.getText();
+        description = txtAreaDescription.getText();
+        type = (String) comboBoxType.getValue();
+        entryFee = Float.parseFloat(txtEntryFee.getText());
+        capacity = Integer.parseInt(txtCapacity.getText());
+        startDate = Date.valueOf(dpStartDate.getValue());
+        endDate = Date.valueOf(dpEndDate.getValue());  
+        
+        Event ev = new Event(name, location, type, description, entryFee, capacity, startDate, endDate, userID);
+
+        es = new EventService();
+
+        int result = es.createEvent(ev);
+
+        if (result > 0) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Add Event");
+            alert.setHeaderText(null);
+            alert.setContentText("A new event has been added successfully!");
+            alert.showAndWait();
+            userID++;
+            alert.close();
+            returnToEventHomepage(event);
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Add Custom Product");
+            alert.setHeaderText(null);
+            alert.setContentText("Failed to add event!");
+            alert.showAndWait();
+        }
+    }
+
 
 }
