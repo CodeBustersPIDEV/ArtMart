a92c6470bc513f85286a571d4e19c74ce27ab784
MarwenTouati1 <marwen.touati@esprit.tn>
2023-02-23 10:36:17 +0100

    edit ui

--- ArtMart_JAVA_Build/src/com/artmart/GUI/controllers/Blog/CommentCardController.java
+++ ArtMart_JAVA_Build/src/com/artmart/GUI/controllers/Blog/CommentCardController.java
@@ -13,12 +13,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.AccessibleRole;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
@@ -40,16 +41,19 @@ public class CommentCardController implements Initializable {
     private Label comment_content;
     @FXML
     private Label commentID;
+    @FXML
+    private Pane btnPane;
+    @FXML
+    private MenuButton operations;
 
     private final BlogService blogService = new BlogService();
     private final UserDao userService = new UserDao();
     private Comment viewComment = new Comment();
 
     private int id;
-    @FXML
-    private Pane btnPane;
-    @FXML
-    private ComboBox<String> operations;
+
+    MenuItem menuItem1;
+    MenuItem menuItem2;
 
     /**
      * Initializes the controller class.
@@ -77,10 +81,24 @@ public class CommentCardController implements Initializable {
         File file = new File("src/com/artmart/assets/BlogAssets/menu.png");
         Image image = new Image(file.toURI().toString());
         this.iconBtn.setImage(image);
-        MenuItem menuItem1 = new MenuItem("Edit");
-        MenuItem menuItem2 = new MenuItem("Delete");
+        menuItem1 = new MenuItem("Edit");
+        TextArea editComment_content=new TextArea();
+        editComment_content.setWrapText(true);
+        menuItem1.setOnAction(e -> {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Edit Your Comment");
+            alert.setHeaderText(null);
+            alert.setWidth(20);
+            alert.setHeight(150);
+            alert.setGraphic(editComment_content);
+            alert.showAndWait();
 
-        this.operations.getItems().addAll(menuItem1.getText(), menuItem2.getText());
+        });
+        menuItem2 = new MenuItem("Delete");
+
+//        this.operations.getItems().addAll(menuItem1.getText(), menuItem2.getText());
+        this.operations.getItems().addAll(menuItem1,menuItem2);
+
         this.iconBtn.setOnMouseClicked(e -> {
             this.operations.show();
         });
