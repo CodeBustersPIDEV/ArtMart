5e06fa1aa4e535970dd650849f733bd36d52e2fb
M3herBenSalem <meher.bensalem@esprit.tn>
2023-03-09 22:38:21 +0100

    Update ArtistReadyProductsListController.java

--- ArtMart_JAVA_Build/src/com/artmart/GUI/controllers/Product/ArtistReadyProductsListController.java
+++ ArtMart_JAVA_Build/src/com/artmart/GUI/controllers/Product/ArtistReadyProductsListController.java
@@ -286,6 +286,8 @@ public class ArtistReadyProductsListController implements Initializable {
             Parent root = loader.load();
             Scene scene = new Scene(root);
             stage.setResizable(false);
+        } catch (IOException ex) {
+            Logger.getLogger(ArtistReadyProductsListController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
